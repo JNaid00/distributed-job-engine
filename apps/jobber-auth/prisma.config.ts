2d2fdd69baf062e4464099c2eedf71d7aabf616a
Jesse Naidoo <jessenaidoo24@gmail.com>
2025-12-15 16:33:19 +0200

    init prisma

---
+++ apps/jobber-auth/prisma.config.ts
@@ -0,0 +1,9 @@
+import 'dotenv/config'; // load .env
+import { defineConfig, env } from 'prisma/config';
+
+export default defineConfig({
+  datasource: {
+    url: env('AUTH_DATABASE_URL'),
+  },
+  schema: 'prisma/schema.prisma',
+});
