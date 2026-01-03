37989064fc8da42c67aa519193b4eb7652570acc
Jesse Naidoo <jessenaidoo24@gmail.com>
2026-01-03 14:59:49 +0200

    Pulsar consumer

--- libs/pulsar/src/lib/pulsar.client.ts
+++ libs/pulsar/src/lib/pulsar.client.ts
@@ -7,16 +7,23 @@ export class PulsarClient implements OnModuleDestroy {
   private readonly client = new Pulsar.Client({
     serviceUrl: this.configService.getOrThrow<string>('PULSAR_SERVICE_URL'),
   });
+  private readonly producers: Pulsar.Producer[] = [];
 
   constructor(private readonly configService: ConfigService) {}
 
   async createProducer(topic: string) {
-    return this.client.createProducer({
+    const producer: Pulsar.Producer = await this.client.createProducer({
       topic: topic,
     });
+    this.producers.push(producer);
+
+    return producer;
   }
 
   async onModuleDestroy() {
+    this.producers.forEach(prod => {
+        prod.close();
+    })
     await this.client.close();
   }
 }
