5e6b2717f4d130b8091877301387845c66bb407c
Jesse Naidoo <jessenaidoo24@gmail.com>
2026-01-03 15:43:54 +0200

    setup executer

--- apps/jobber-jobs1/src/main.ts
+++ apps/jobber-jobs1/src/main.ts
@@ -16,7 +16,7 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.setGlobalPrefix(globalPrefix);
   app.use(cookieParser());
-  const port = app.get(ConfigService).getOrThrow('JOBBER_1_PORT') || 3000;
+  const port = app.get(ConfigService).getOrThrow('PORT');
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
